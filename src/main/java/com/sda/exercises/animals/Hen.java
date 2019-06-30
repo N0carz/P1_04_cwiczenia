2b029d163da3aa096186d276905b5ea75fa04ba6
Eugeniusz Pasicznyk <chonest@wp.pl>
2019-06-30 14:03:04 +0200

    added foods added animals

---
+++ src/main/java/com/sda/exercises/animals/Hen.java
@@ -0,0 +1,11 @@
+package com.sda.exercises.animals;
+
+import static com.sda.exercises.food.Foods.BONES;
+
+public class Hen extends Animal {
+
+    public Hen() {
+        super();
+        favoriteFoods.add(BONES);
+    }
+}
